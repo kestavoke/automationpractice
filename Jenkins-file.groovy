509ddc2189b73fab1760c9d3bec4038f362d6ecc
Kestavoke <talktovoke@gmail.com>
2020-12-17 01:43:11 +0200

    Add Jenkins-file.groovy

---
+++ Jenkins-file.groovy
@@ -0,0 +1,34 @@
+node {
+    stage('Git checkout') { // for display purposes
+        git 'https://kestavoke@bitbucket.org/kestavoke/automationpractice.git'
+    }
+    stage('Smoke') {
+        try {
+            sh "mvn clean verify -Dtags='type:Smoke'"
+        } catch (err) {
+
+        } finally {
+            publishHTML (target: [
+                    reportDir: 'target/site/serenity',
+                    reportFiles: 'index.html',
+                    reportName: "Smoke tests report"
+            ])
+        }
+    }
+    stage('Regression') {
+        try {
+            sh "mvn verify -Dtags='type:Regression'"
+        } catch (err) {
+
+        } finally {
+            publishHTML (target: [
+                    reportDir: 'target/site/serenity',
+                    reportFiles: 'index.html',
+                    reportName: "Regression tests report"
+            ])
+        }
+    }
+    stage('Results') {
+        junit '**/target/failsafe-reports/*.xml'
+    }
+}
