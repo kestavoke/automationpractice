87582695380ef6571744fcf7083b8ea6ed74f5b0
Kestavoke <talktovoke@gmail.com>
2020-12-17 03:12:13 +0200

    Add Jenkins-file.groovy

--- Jenkins-file.groovy
+++ Jenkins-file.groovy
@@ -1,4 +1,7 @@
 node {
+    tools {
+        maven 'M3'
+    }
     stage('Git checkout') { // for display purposes
         git 'https://kestavoke@bitbucket.org/kestavoke/automationpractice.git'
     }
